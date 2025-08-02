fa9a8f2e069dada223caf9e1afa743356b172d90
ramak8sgcp <ramak8sgcp@gmail.com>
2025-08-02 20:21:46 +0530

    conversation

---
+++ shell-script/04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "${PERSON1}:: Hi ${PERSON2}, how are you?"
+echo "${PERSON2}:: Hi ${PERSON1}, I am good, how are you?"
+echo "${PERSON1}:: I am doing Good ${PERSON2}. What's going on?"
+echo "${PERSON2}:: I started learing Shell script ${PERSON1}"
