c6e54aa1154ae487bd8c745354edac3ad52fd2fa
ramak8sgcp <ramak8sgcp@gmail.com>
2025-08-02 20:12:09 +0530

    conversation

---
+++ shell-script/02-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# author is Ramana
+# Class is devops
+
+echo "Ramana:: Hi Obulesh, how are you?"
+echo "Obulesh:: Hi Ramana, I am good, how are you?"
+echo "Ramana:: I am doing Good Obulesh. What's going on?"
+echo "Obulesh:: I started learing Shell script Ramana"
