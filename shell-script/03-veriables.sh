8250da55027241bb5688c36da0922cb61b4d9301
ramak8sgcp <ramak8sgcp@gmail.com>
2025-08-02 20:16:49 +0530

    conversation

---
+++ shell-script/03-veriables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "PERSON1:: Hi PERSON2, how are you?"
+echo "PERSON2:: Hi PERSON1, I am good, how are you?"
+echo "PERSON1:: I am doing Good PERSON2. What's going on?"
+echo "PERSON2:: I started learing Shell script PERSON1"
