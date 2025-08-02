aec1bdd8485c8e6512edb3daba95da001241cb4a
ramak8sgcp <ramak8sgcp@gmail.com>
2025-08-02 20:27:46 +0530

    conversation

---
+++ shell-script/05-variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Please enter youname:"
+
+read USERNAME 
+
+echo "Username entered is: $USERNAME"
+
+echo "Please enter your password:
+
+read PASSWORD
+
+echo "Password entered is : $PASSWORD"
