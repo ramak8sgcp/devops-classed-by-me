65cfcf57fcdc7daf9a4e4cefef698e22287d05fb
ramak8sgcp <ramak8sgcp@gmail.com>
2025-08-06 11:43:26 +0530

    installation

--- shell-script/10-condition.sh
+++ shell-script/10-condition.sh
@@ -2,7 +2,7 @@
 
 NUMBER=$1 
 
-if [ $NUMBER -gt 20 ]
+if [ $NUMBER -gt 20 ] #gt, lt, eq, -ne , -ge, -le
 then
     echo "Given number: $NUMBER is greater than 20"
 else
---
+++ shell-script/11-installation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+USERID=$(id -u)
+#echo "user ID is : $USERID"
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please run this script with root priveleges"
+    exit 1
