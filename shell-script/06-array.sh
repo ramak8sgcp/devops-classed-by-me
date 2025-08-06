380109f0ccf8d90defa0825dd2210c6ec96bca4a
ramak8sgcp <ramak8sgcp@gmail.com>
2025-08-06 10:25:25 +0530

    array

---
+++ shell-script/06-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# index start from 0, size is 3
+FRUITS=("Apple" "kiwi" "Orange")    #Array
+
+echo "First Fruit is: ${FRUITS[0]}"
+echo "Second fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"
+
+echo "Total Fruits are: ${FRUITS[@]}"
