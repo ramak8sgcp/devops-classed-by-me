c6d91d6719ebbb9ee38f38fb4ce1602e2801ce1d
ramak8sgcp <ramak8sgcp@gmail.com>
2025-08-07 09:42:39 +0530

    function

---
+++ shell-script/12-Functions.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please run this script with root priveleges"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "COMMAND is ... FAILED"
+        exit 1
+    else
+        echo "COMMAND is ... SUCCESS"
+    fi
+}
+
+# dnf list installed git
+# if [ $? -ne 0 ]
+# then 
+#     echo "Git is  not installed , going to install it"
+#     dnf install git -y
+#     if [ $? -ne 0 ]
+#     then 
+#         echo "Git installation is not success... Check it"
+#         exit 1
+#     else
+#         echo "Git installation is success"
+#     fi
+# else    
+#     echo "Git is already installed, nothing to do"
+# fi
+
+# # mysql installation
+# dnf list installed mysql
+# if [ $? -ne 0 ]
+# then 
+#     echo "mysql is  not installed , going to install it"
+#     dnf install mysql -y
+#     if [ $? -ne 0 ]
+#     then 
+#         echo "mysql installation is not success... Check it"
+#         exit 1
+#     else
+#         echo "mysql installation is success"
+#     fi
+# else    
+#     echo "mysql is already installed, nothing to do"
+# fi
