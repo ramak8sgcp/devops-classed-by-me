e0b081498ea96d496c57ec23fb79d8ccc4b6346b
ramak8sgcp <ramak8sgcp@gmail.com>
2025-08-07 11:13:44 +0530

    function

--- shell-script/16-redirections.sh
+++ shell-script/16-redirections.sh
@@ -34,7 +34,7 @@ CHECK_ROOT
 
 for package in $@
 do
-    dnf list installed $package
+    dnf list installed $package &>> $LOG_FILE
     if [ $? -ne 0 ]
     then 
         echo "$package is  not installed , going to install it"
