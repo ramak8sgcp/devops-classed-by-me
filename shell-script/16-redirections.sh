38c32c8f1e7532a7bb9445131e6d1d43e4afa312
ramak8sgcp <ramak8sgcp@gmail.com>
2025-08-08 10:52:36 +0530

    redirections update

--- shell-script/16-redirections.sh
+++ shell-script/16-redirections.sh
@@ -3,7 +3,7 @@
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log
 mkdir -p $LOGS_FOLDER
 
 USERID=$(id -u)
@@ -23,10 +23,10 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is ... $R FAILED $N" &>>$LOG_FILE
+        echo -e "$2 is ... $R FAILED $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$2 is ... $G SUCCESS $N" &>>$LOG_FILE
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi  
 }
 
@@ -44,12 +44,12 @@ fi
 
 for package in $@
 do
-    dnf list installed $package &>>$LOG_FILE
+    dnf list installed $package | tee -a $LOG_FILE
     if [ $? -ne 0 ]
     then 
         echo "$package is  not installed , going to install it"
         dnf install $package -y &>>$LOG_FILE
-        VALIDATE $? "Installing $package"
+        VALIDATE $? "Installing $package" | tee -a $LOG_FILE
     else    
         echo "$package is already installed, nothing to do"
     fi
