b8b4fc4ede9051355e9a2f267830b661498448ab
ramak8sgcp <ramak8sgcp@gmail.com>
2025-08-08 10:31:52 +0530

    redirections update

--- shell-script/16-redirections.sh
+++ shell-script/16-redirections.sh
@@ -23,10 +23,10 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is ... $R FAILED $N" | tee -a $LOG_FILE
+        echo -e "$2 is ... $R FAILED $N" &>> $LOG_FILE
         exit 1
     else
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+        echo -e "$2 is ... $G SUCCESS $N" &>> $LOG_FILE
     fi  
 }
 
@@ -34,11 +34,11 @@ CHECK_ROOT
 
 for package in $@
 do
-    dnf list installed $package | tee -a $LOG_FILE
+    dnf list installed $package &>> $LOG_FILE
     if [ $? -ne 0 ]
     then 
         echo "$package is  not installed , going to install it"
-        dnf install $package -y
+        dnf install $package -y &>> $LOG_FILE
         VALIDATE $? "Installing $package"
     else    
         echo "$package is already installed, nothing to do"
