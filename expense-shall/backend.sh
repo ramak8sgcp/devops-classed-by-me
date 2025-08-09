3685222a44f1480dc1c0c74757a162d7f8746707
ramak8sgcp <ramak8sgcp@gmail.com>
2025-08-09 19:27:40 +0530

    backend

--- expense-shall/backend.sh
+++ expense-shall/backend.sh
@@ -34,13 +34,13 @@ echo " script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>LOG_FILE
 VALIDATE $? "Disable default nodejs"
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y &>>LOG_FILE
 VALIDATE $? "Enable nodejs:20"
 
-dnf install nodejs -y
+dnf install nodejs -y &>>LOG_FILE
 VALIDATE $? "Install nodejs"
 
 id expense
@@ -58,14 +58,35 @@ fi
 mkdir /app
 VALIDATE $? "Creating /app folder"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>LOG_FILE
 VALIDATE $? "Download backend application code"
 
 cd /app
 rm -rf /app/*
 unzip /tmp/backend.zip
-VALIDATE $? "Extracting backend application code"
+VALIDATE $? "Extracting backend application code" &>>LOG_FILE
 
-npm install
+npm install &>>LOG_FILE
+
+cp /home/ec2-user/devops-classed-by-me/expense-shall/backend.service /etc/systemd/system/backend.service
+
+dnf install mysql -y &>>LOG_FILE
+VALIDATE $? "Installing mysql"
+
+mysql -h mysql.ramana3490.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+VALIDATE $? "loading schema"
+
+systemctl daemon-reload &>>LOG_FILE
+VALIDATE $? "daemon-reload"
+
+systemctl start backend &>>LOG_FILE
+VALIDATE $? "Start backend"
+
+systemctl enable backend &>>LOG_FILE
+VALIDATE $? "Enabled backend"
+
+
+
+
 
 
